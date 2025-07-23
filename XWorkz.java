fa4e4243a31a3c0a51f36df47867d51f6a124f4e
Shwethabasavaraj <shwetha26082000@gmail.com>
2025-07-23 11:45:12 +0530

    deleting arrays related files

---
+++ XWorkz.java
@@ -0,0 +1,40 @@
+class XWorkz{
+
+	static String instituteName;
+   //var_type      variable        ref
+   
+	static String firstClass;
+	static String conductedBy;
+	
+	
+	public static void main(String institute[]){
+		
+		 instituteName ="X-Workz";
+		 firstClass ="CoreJava";
+		 conductedBy ="Mr.Dev";
+		 
+		String timing ="10:00am - 1:00pm";
+		System.out.println("Timing of Class :" +" "+timing);
+		timing ="2:00pm - 4:00pm";
+		
+		int noOfStaff=5;
+		System.out.println("No of Staff :" +" "+noOfStaff);
+		noOfStaff=10;
+		
+	    int noOfStudents=150;
+		System.out.println("No of Students :" +" "+noOfStudents);
+		noOfStudents=200;
+	    
+		
+		
+	System.out.println("InstituteName :" +" "+instituteName);
+	System.out.println("Timing of Class :" +" "+timing);
+	System.out.println("No of Staff :"+" "+noOfStaff);
+	System.out.println("No of Students :"+" "+noOfStudents);
+	System.out.println("First Class :"+" "+firstClass);
+	System.out.println("Conducted by :"+" "+conductedBy);
+	
+	}
+	
+	
+}
